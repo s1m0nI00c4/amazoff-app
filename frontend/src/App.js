02b7261eb46fb6e9eb05b909bc9e878c1aba93ef
s1m0nI00c4 <43318934+s1m0nI00c4@users.noreply.github.com>
2021-01-21 18:40:30 +0100

    Create frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="/">amazoff</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign in</a>
+                </div>
+            </header>
+            <main>
+                <Route exact path="/" component={HomeScreen}></Route>
+                <Route path="/product/:id" component={ProductScreen}></Route>
+            </main>
+            <footer className="row center">(2021) - All rights reserved</footer>
+        </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+export const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/product-1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '2',
+            name: 'Puma fit shirt',
+            category: 'Shirts',
+            image: '/images/product-2.jpg',
+            price: 100,
+            countInStock: 1,
+            brand: 'Puma',
+            rating: 4,
+            numReviews: 1,
+            description: 'High quality product'
+        },
+        {
+            _id: '3',
+            name: 'Levis slim shirt',
+            category: 'Shirts',
+            image: '/images/product-3.jpg',
+            price: 120,
+            countInStock: 0,
+            brand: 'Levis',
+            rating: 3.5,
+            numReviews: 20,
+            description: 'High quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike slim pants',
+            category: 'Pants',
+            image: '/images/product-4.jpg',
+            price: 150,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 90,
+            description: 'High quality product'
+        },
+        {
+            _id: '5',
+            name: 'Puma slim pants',
+            category: 'Pants',
+            image: '/images/product-5.jpg',
+            price: 90,
+            countInStock: 20,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'High quality product'
+        },
+        {
+            _id: '6',
+            name: 'Levis fit pants',
+            category: 'Pants',
+            image: '/images/product-6.jpg',
+            price: 120,
+            countInStock: 5,
+            brand: 'Levis',
+            rating: 5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+    ]
+}
