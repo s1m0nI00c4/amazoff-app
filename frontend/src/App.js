84196759b74b550eb1108a8be7e2cf2c199fa634
s1m0nI00c4 <43318934+s1m0nI00c4@users.noreply.github.com>
2021-01-24 20:13:28 +0100

    signin form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Link, Route} from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector(state => state.cart);
@@ -27,6 +28,7 @@ function App() {
             <main>
                 <Route exact path="/" component={HomeScreen}></Route>
                 <Route path="/products/:id" component={ProductScreen}></Route>
+                <Route path="/signin" component={SigninScreen}></Route>
                 <Route path="/cart/:id?" component={CartScreen}></Route>
             </main>
             <footer className="row center">(2021) - All rights reserved</footer>
