ffb47d1be067005a509c642267117e23feca0f11
s1m0nI00c4 <43318934+s1m0nI00c4@users.noreply.github.com>
2021-01-25 12:15:02 +0100

    signin function introduced and redirects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -9,6 +10,13 @@ import SigninScreen from './screens/SigninScreen';
 function App() {
   const cart = useSelector(state => state.cart);
   const {cartItems} = cart;
+  const userSignin = useSelector(state => state.userSignin);
+  const {userInfo} = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  }
+
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -22,7 +30,21 @@ function App() {
                       <span className="badge">{cartItems.length}</span>
                     )}
                     </Link>
-                    <Link to="/signin">Sign in</Link>
+                    {
+                      userInfo ? (
+                        <div className="dropdown">
+                          <Link to="#">
+                            {userInfo.name}<span> </span>
+                            <i className="fa fa-caret-down"></i>
+                          </Link>
+                          <ul className="dropdown-content">
+                            <Link to="#signout" onClick={signoutHandler}>Sign out</Link>
+                          </ul>
+                        </div>
+                      ) : (
+                        <Link to="/signin">Sign in</Link>
+                      )
+                    }
                 </div>
             </header>
             <main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,16 +2,21 @@ import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { userSignInReducer } from './reducers/userReducer';
 
 const initialState = {
     cart: {
         cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+    },
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
     }
 };
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userSignin: userSignInReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
